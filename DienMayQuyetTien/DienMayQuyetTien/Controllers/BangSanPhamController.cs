2a92276ee47dd6018567dc3f89c2817a232859c3
Hoai Phat <hoaiphat0206@gmail.com>
2018-11-28 09:53:55 +0700

    Upload Image

--- DienMayQuyetTien/DienMayQuyetTien/Controllers/BangSanPhamController.cs
+++ DienMayQuyetTien/DienMayQuyetTien/Controllers/BangSanPhamController.cs
@@ -23,18 +23,25 @@ namespace DienMayQuyetTien.Controllers
         }
 
         // GET: /BangSanPham/Details/5
-        public ActionResult Details(int? id)
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    BangSanPham bangsanpham = db.BangSanPhams.Find(id);
+        //    if (bangsanpham == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(bangsanpham);
+        //}
+
+        public FileResult Details(string id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
-            if (bangsanpham == null)
-            {
-                return HttpNotFound();
-            }
-            return View(bangsanpham);
+            var path = Server.MapPath("~/App_Data");
+            path = System.IO.Path.Combine(path, id);
+            return File(path, "image");
         }
 
         // GET: /BangSanPham/Create
